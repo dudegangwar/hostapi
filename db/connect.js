7f9fa3e8a28735cccc017cae3343d0b88e72e565
Anuj Kumar <86113409+dudegangwar@users.noreply.github.com>
2024-02-09 20:06:46 +0530

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-// uri = "mongodb+srv://dudegangwar:GHoop389!!@collegeapi.qbsuqye.mongodb.net/collegeAPI?retryWrites=true&w=majority";
 
 const connectDB = (uri) => {
     console.log("Connected to the database");
